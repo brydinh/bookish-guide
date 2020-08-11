de79ffcb2774fc13c7c61e20f8185c1e621dd6db
Brian Dinh <bdinh98@gmail.com>
2020-08-11 18:03:20 -0400

    Add delete functionality + animation

--- public/config/config.js
+++ public/config/config.js
@@ -1,5 +1,5 @@
 var Keys = {
   key1s: ["Accord", "Civic", "HR-V", "Odyssey", "Insight", "Pilot", "Passport"],
   key2s: ["V4", "V6", "V8"],
-  values: ["Model1", "Model2", "Model3", "Model4", "Model1-6", "Model2-6"]
+  values: ["Model1", "Model2", "Model3", "Model4", "Model1-6", "Model2-6", "Cv4-1", "Cv4-2", "Cv4-3"]
 }
--- src/components/ConfigList.jsx
+++ src/components/ConfigList.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import FlipMove from "react-flip-move";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Configurations() {
   library.add(faTrash);
@@ -22,19 +25,43 @@ function Configurations() {
     fetchData();
   });
 
+// TODO: Add confirmation dialog
+  function deleteConfig(id) {
+    axios.delete("/configs/"+parseInt(id))
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  function editConfig(id){
+    console.log("Edit config");
+    console.log(id);
+  }
+
   return (
     <div className="list">
       <h2>Configurations</h2>
-      <ul>
-        {configurations.map(config =>
-        <div className="listItem" key = {config.config_id}>
-          <p>
-            {config.key1}: {config.key2} ({config.minfloat} - {config.maxfloat}) {config.value}
-            <span><FontAwesomeIcon className ="editIcon" icon="edit"/></span>
-            <span><FontAwesomeIcon className ="trashIcon" icon="trash"/></span>
-          </p>
-        </div>)}
-      </ul>
+        <ul>
+        <FlipMove leaveAnimation="accordionVertical">
+          {configurations.map(config =>
+            <div className="listItem" key = {config.config_id}>
+              <p>
+                {config.key1}: {config.key2} ({config.minfloat} - {config.maxfloat}) {config.value}
+                <span>
+                  <FontAwesomeIcon className ="editIcon" icon="edit" onClick={() => editConfig(config.config_id)}/>
+                </span>
+                <span>
+                  <FontAwesomeIcon className ="trashIcon" icon="trash" onClick={() => deleteConfig(config.config_id)}/>
+                </span>
+             </p>
+          </div>
+        )}
+        </FlipMove>
+
+        </ul>
     </div>
   )
 }

2cbd10bd6bdfd709acf7b8ff2a20251899942bc9
Brian Dinh <bdinh98@gmail.com>
2020-08-11 16:35:51 -0400

    Rework Continue

--- src/components/ConfigList.jsx
+++ src/components/ConfigList.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Configurations() {
+  library.add(faTrash);
+  library.add(faEdit);
+
   const [configurations, setConfigurations] = useState([]);
 
   async function fetchData() {
@@ -19,10 +27,14 @@ function Configurations() {
       <h2>Configurations</h2>
       <ul>
         {configurations.map(config =>
-        <li key = {config.config_id}>
-          {config.key1}: {config.key2} ({config.minfloat} - {config.maxfloat}) {config.value}
-        </li>)}
-    </ul>
+        <div className="listItem" key = {config.config_id}>
+          <p>
+            {config.key1}: {config.key2} ({config.minfloat} - {config.maxfloat}) {config.value}
+            <span><FontAwesomeIcon className ="editIcon" icon="edit"/></span>
+            <span><FontAwesomeIcon className ="trashIcon" icon="trash"/></span>
+          </p>
+        </div>)}
+      </ul>
     </div>
   )
 }

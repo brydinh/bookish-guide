383d12e2b5ae2643677f461c07388a5f202ce9aa
Brian Dinh <bdinh98@gmail.com>
2020-12-13 02:08:06 -0500

    Change name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1 className="header">M15ty Control Pad</h1>
+      <h1 className="header">Valve Automation 3000</h1>
     </header>
   );
 }
